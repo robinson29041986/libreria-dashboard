f12f51866e31d6cfea1f8de5a1ffe77fb0be40d4
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2023-03-30 22:46:07 -0500

    little fix

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import { Close } from '@easy-eva-icons/react';
+import { CloseOutline } from '@easy-eva-icons/react';
 
 const ConfirmationModal = (props) => {
   return ReactDOM.createPortal(
@@ -7,7 +7,7 @@ const ConfirmationModal = (props) => {
       <div className="modal_box" role="dialog" aria-modal="true">
         <div className="modal_header">
           <h4 className="subheading">{props.title}</h4>
-          <Close className="modal_icon" onClick={props.onClose} />
+          <CloseOutline className="modal_icon" onClick={props.onClose} />
         </div>
         <div className="modal_content">
           <p>{props.message}</p>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom"
-import { Close } from "@easy-eva-icons/react";
+import { CloseOutline } from "@easy-eva-icons/react";
 
 const Modal = ({ isOpen, onClose, title, children }) => isOpen ? ReactDOM.createPortal(
   <div className="modal">
     <div className="modal_box" role="dialog" aria-modal="true">
       <div className="modal_header">
         <h4 className="subheading">{title}</h4>
-        <Close className="modal_icon" onClick={onClose} />
+        <CloseOutline className="modal_icon" onClick={onClose} />
       </div>
       <div className="modal_content">
         {children}
